a37fa816632cd7e27b9e6e92e06a3337e8b085a8
sahil1126 <sahildate26@gmail>
2026-01-31 14:21:30 +0530

    Allow Assets and assest folders in SecurityConfig for static resource serving

--- pharma-backend/src/main/java/com/kavyapharm/farmatrack/config/SecurityConfig.java
+++ pharma-backend/src/main/java/com/kavyapharm/farmatrack/config/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
                                 "/login.html",
                                 "/signup.html",
                                 "/assets/**",
+                                "/Assets/**",
+                                "/assest/**",
                                 "/**/*.js",
                                 "/**/*.css",
                                 "/**/*.png",
