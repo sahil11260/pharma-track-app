81d9f0b0aba031dadaa5e37c9298d760e58cecab
Nikita-Garule05 <mahiradha818@gmail.com>
2026-02-05 11:34:11 +0530

    Test write access

--- Final_KavyaPharma/Pharma_Final_Projectt (2)/Pharma_Final_Projectt/Pharma_Final_Project/Backend/src/main/java/com/kavyapharm/farmatrack/distribution/repository/DistributionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kavyapharm.farmatrack.distribution.repository;
-
-import com.kavyapharm.farmatrack.distribution.model.Distribution;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DistributionRepository extends JpaRepository<Distribution, Long> {
-}
--- Final_KavyaPharma/Pharma_Final_Projectt (2)/Pharma_Final_Projectt/Pharma_Final_Project/Backend/src/main/java/com/kavyapharm/farmatrack/notification/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kavyapharm.farmatrack.notification.repository;
-
-import com.kavyapharm.farmatrack.notification.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, String> {
-}
